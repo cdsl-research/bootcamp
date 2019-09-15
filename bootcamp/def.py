a536f52921c4acc06c65359ed5e4963a810480d0
mf-yoshi <c011703961@edu.teu.ac.jp>
2019-09-16 05:26:43 +0900

    bootcamp

--- bootcamp/def.py
+++ bootcamp/def.py
@@ -1,4 +1,4 @@
-import math
+import random
 
 def add(x,y):
     print(x+y)
@@ -12,13 +12,19 @@ def mul(x,y):
 def div(x,y):
     print(x / y)
 
-def area(x):
-    print(x*x*math.pi)
+def ran1():
+    print(random.random())
+
+def ran2(x,y):
+    print(random.randint(x,y))
+
+def ran3(x,y):
+    print(random.uniform(x,y))
 
 add(3,5)
 sub(3,5)
 mul(3,5)
 div(3,5)
-
-area(5)
-
+ran1()
+ran2(1,10)
+ran3(1,10)
