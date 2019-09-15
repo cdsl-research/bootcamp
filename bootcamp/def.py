52af8ecaec32431b17f4c81b5960c1ae741b2007
mf-yoshi <c011703961@edu.teu.ac.jp>
2019-09-16 05:20:30 +0900

    bootcamp

---
+++ bootcamp/def.py
@@ -0,0 +1,24 @@
+import math
+
+def add(x,y):
+    print(x+y)
+
+def sub(x,y):
+    print(x - y)
+
+def mul(x,y):
+    print(x * y)
+
+def div(x,y):
+    print(x / y)
+
+def area(x):
+    print(x*x*math.pi)
+
+add(3,5)
+sub(3,5)
+mul(3,5)
+div(3,5)
+
+area(5)
+
