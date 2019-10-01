aab1a92a084f645d2dfd0956353ec1e424ebf090
mf-yoshi <53615232+mf-yoshi@users.noreply.github.com>
2019-10-01 18:31:36 +0900

    Update slack-test.py

--- bootcamp/slack-test.py
+++ bootcamp/slack-test.py
@@ -1,12 +1,12 @@
 #import slackweb
 
-#slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+#slack = slackweb.Slack(url="webhookで取得したURL")
 #slack.notify(text="pythonからslackさんへ")
 
 import requests
 import json
 
-requests.post('https://hooks.slack.com/services/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', data = json.dumps({
+requests.post('webhookで取得したURL', data = json.dumps({
     'text': u'Hello,World!', # 投稿するテキスト
     'username': u'yoshiki', # 投稿のユーザー名
     'icon_emoji': u':ghost:', # 投稿のプロフィール画像に入れる絵文字
